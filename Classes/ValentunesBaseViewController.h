167e7ad97aff057998a25a6f9094cd30be791732
Anna Callahan <annacallahan@gmail.com>
2011-06-08 01:03:52 +0200

    switch email to username; lose spinner & logging in text

---
+++ Classes/ValentunesBaseViewController.h
@@ -0,0 +1,16 @@
+//
+//  ValentunesBaseViewController.h
+//  Valentunes
+//
+//  Created by Anna Callahan on 6/7/11.
+//  Copyright 2011 SuperIndieFilms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ValentunesBaseViewController : UIViewController {
+    
+}
+
+@end
